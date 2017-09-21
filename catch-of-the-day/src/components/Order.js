87efd69f78754b6b820faabd03b81bad779cdda9
Ryan DeLosh <ryguy1201@gmail.com>
2017-09-21 17:49:59 -0400

    After Vid 16

---
+++ catch-of-the-day/src/components/Order.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Order extends React.Component {
+  render() {
+    return (
+      <h3>Order</h3>
+    )
+  }
+}
+
+export default Order;
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,25 @@
-// let's go!
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Match, Miss } from 'react-router';
+
+
+import './css/style.css';
+
+
+import StorePicker from './components/StorePicker';
+import App from './components/App';
+import notFound from './components/notFound';
+
+const Root = () => {
+	return (
+		<BrowserRouter>
+			<div>
+				<Match exactly pattern="/" component={StorePicker} />
+				<Match pattern="/store/:storeId" component={App} />
+				<Miss component={notFound} />
+			</div>
+		</BrowserRouter>
+	)
+}
+
+ReactDOM.render(<Root/>, document.querySelector('#main'));
